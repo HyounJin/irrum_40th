8b0aef171349a9bae7a1492b441fa2f2b097a2c2
HyounJin <51051489+HyounJin@users.noreply.github.com>
2021-03-09 16:25:51 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -88,11 +88,11 @@ const goResult = () => {
 
   pTitle.innerHTML = u_name.value + ' 님은...';
   res_point.innerHTML = point + '점';//점수표시
-  //pin.style.marginLeft = infoList[grade].mLeft;
-  //res_img.src = img_url;
-  //res_img.alt = infoList[grade].name;
-  //res_img.title = infoList[grade].name;
-  //res_img_div.appendChild(res_img);
+  pin.style.marginLeft = infoList[grade].mLeft;
+  res_img.src = img_url;
+  res_img.alt = infoList[grade].name;
+  res_img.title = infoList[grade].name;
+  res_img_div.appendChild(res_img);
   animal.innerHTML = infoList[grade].name;//커다란 설명
   desc.innerHTML = infoList[grade].desc;//자세한 설명
 
