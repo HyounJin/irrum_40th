51b8b9530080c742f9a7642023ad49146d120c0c
HyounJin <51051489+HyounJin@users.noreply.github.com>
2021-03-11 04:10:56 +0900

    result image margin fix
    2021 0311 04:10

--- js/share.js
+++ js/share.js
@@ -1,6 +1,6 @@
 const url = 'https://hyounjin.github.io/irrum_40th/';
 const title = '[동아리 분과 테스트] 찰떡궁합 동아리 ';
-const hash = '%23irrum_40th %233분동아리테스트 %23동아리분과테스트 ';
+const hash = '%23iruum_40th %23꿈은이룸어진다 %23동아리분과테스트 ';
 
 const fb = () => {
   window.open('https://www.facebook.com/sharer/sharer.php?u='
@@ -16,10 +16,10 @@ const tw = () => {
       name += '이에요! ';
       break;
     default:
-      name += '예요! ';
+      name += '이에요! ';
   }
   window.open('https://twitter.com/intent/tweet?text='
-    + title + '%0A' + '저는 ' + name + '' + hash + '%0A' + url,
+    + title + '%0A' + name + '' + hash + '%0A' + url,
     'twittersharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
   return false;
 }
